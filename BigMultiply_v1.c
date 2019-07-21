36dd6f344710181657a6006dd6d9f95a191c53ab
Alex <al.nezhurenko@gmail.com>
2019-07-21 18:32:53 +0300

    First commit

---
+++ BigMultiply_v1.c
@@ -0,0 +1,23 @@
+#include "include/check.h"
+#include "include/multiply.h"
+
+// Main code
+int main(int argc, char **argv)
+{
+
+    if (argc < 3 || argc >= 4)
+    {
+		puts("Please Provide Two Unsigned Integers as arguments");
+		return 1;
+	}
+
+    if (check(argv[1]) == 0 || check(argv[2]) == 0 )
+    {
+		puts("Wrong Data - Please Provide Only Unsigned Integers as arguments");
+		return 1;
+	}
+
+    multiply(argv[1], argv[2]);
+
+	return 0;
+}
---
+++ include/check.h
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <string.h>
+
+int check(char *str)
+{
+    for (int i = 0; i< strlen(str); i++)
+
+         if (!isdigit(str[i]))
+
+         return 0;
+
+    return 1;
+}
